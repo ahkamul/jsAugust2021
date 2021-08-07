660eb3de2e36179572156df046f032a13d55323a
ahkamul <sasraishougat@gmail.com>
2021-08-07 09:29:21 +0600

    for loop added to index.js at Aug 7, 2021

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var arr = [1,2,3,4,5]
+// var arr = [1,2,3,4,5]
 
-console.log(arr)
-console.log(arr[0])
-console.log(arr[10])
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[10])
 
-arr [2] = 20
+// arr [2] = 20
 
-console.log(arr)
+// console.log(arr)
+
+// for loop: 
+
+for (var i = 1; i < 15; i++){
+    console.log(i)
+}
